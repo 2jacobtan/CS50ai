78a2aa69f16c415481dc74311c265bc31e79f2ba
Jacob Tan <2jacobtan@gmail.com>
2020-07-05 01:02:15 +0800

    Set n_neighbors=1

--- 4a shopping/shopping.py
+++ 4a shopping/shopping.py
@@ -136,7 +136,7 @@ def train_model(evidence, labels):
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    neigh = KNeighborsClassifier()
+    neigh = KNeighborsClassifier(n_neighbors=1)
     
     return neigh.fit(evidence, labels)
 
